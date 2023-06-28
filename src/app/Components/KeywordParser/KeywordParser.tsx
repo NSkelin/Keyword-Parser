10e502eccac31923833cc82310860d75dc57c391
Nick <nick.skelin@gmail.com>
2023-06-28 15:45:27 -0700

    update to use db data and support custom colors

--- src/app/Components/KeywordParser/KeywordParser.tsx
+++ src/app/Components/KeywordParser/KeywordParser.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {useState} from "react";
+import React, {ReactNode, useState, CSSProperties} from "react";
 import HighlightWithinTextarea from "react-highlight-within-textarea";
 import {createKeywordsRegEx} from "utils";
 import KeywordDisplay from "../KeywordDisplay/KeywordDisplay";
@@ -32,33 +32,36 @@ function countKeywords(sourceText: string, keywords: keyword[]) {
   return map;
 }
 
-type KeywordParserProps = {};
-function KeywordParser({}: KeywordParserProps) {
+type HighlightColorProps = {color: CSSProperties["backgroundColor"]; children?: ReactNode};
+const HighlightColor = ({color, children}: HighlightColorProps) => {
+  return <mark style={{backgroundColor: color}}>{children}</mark>;
+};
+
+type KeywordParserProps = {
+  highlightFilters: {
+    color: string | undefined;
+    filter: string[];
+  }[];
+};
+function KeywordParser({highlightFilters}: KeywordParserProps) {
   const [value, setValue] = useState("");
   const onChange = (value: string) => setValue(value);
+
+  let highlights = [];
+  for (const {color, filter} of highlightFilters) {
+    highlights.push({
+      highlight: createKeywordsRegEx(filter),
+      component: ({children}: {children: ReactNode}) => <HighlightColor color={color}>{children}</HighlightColor>,
+    });
+  }
+
+  console.dir(highlights, {depth: null});
   return (
     <>
       <section className={styles.HighlightAreaWrap}>
         <h2>Text to parse</h2>
         <div className={styles.textArea}>
-          <HighlightWithinTextarea
-            value={value}
-            highlight={[
-              {
-                highlight: createKeywordsRegEx(hardSkills.keywords),
-                className: styles.red,
-              },
-              {
-                highlight: createKeywordsRegEx(softSkills.keywords),
-                className: styles.blue,
-              },
-              {
-                highlight: createKeywordsRegEx(otherKeywords.keywords),
-                className: styles.yellow,
-              },
-            ]}
-            onChange={onChange}
-          />
+          <HighlightWithinTextarea value={value} highlight={highlights} onChange={onChange} />
         </div>
       </section>
       <section className={styles.keywordsLists}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import {getCollectionAliases} from "utils/prisma";
 import KeywordParser from "./Components/KeywordParser/KeywordParser";
 import RegExDisplayCollection from "./Components/RegExDisplayCollection/RegExDisplayCollection";
 import styles from "./page.module.scss";
 
-export default function Home() {
+export default async function Home() {
+  const collectionsAliases = await getCollectionAliases();
+  // remove empty collections and unnecessary properties
+  const highlightFilters = collectionsAliases.reduce((filters, {color, keywords}) => {
+    if (keywords.length > 0) filters.push({color: color, filter: keywords});
+    return filters;
+  }, [] as {color: string | undefined; filter: string[]}[]);
+
   return (
     <main className={styles.main}>
       <div className={styles.contentWrap}>
-        <KeywordParser />
+        <KeywordParser highlightFilters={highlightFilters} />
       </div>
       <RegExDisplayCollection />
     </main>
