05fc750877843b579f894ce6b4c11321688a5b5c
Nick <nick.skelin@gmail.com>
2023-06-29 11:35:23 -0700

    update createKeywordsRegEx function Remove redundant code Add option for single strings

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,8 @@
 // creates the regex for highlighting the keywords
-export function createKeywordsRegEx(keywords: string[]) {
-  let arr: string[] = [];
-  for (const keyword of keywords) {
-    arr.push(keyword);
+export function createKeywordsRegEx(keywords: string[] | string) {
+  if (typeof keywords === "string") {
+    return new RegExp(`\\b${keywords}\\b`, "gi");
+  } else {
+    return new RegExp(`\\b${keywords.join("\\b|\\b")}\\b`, "gi");
   }
-
-  return new RegExp(`\\b${arr.join("\\b|\\b")}\\b`, "gi");
 }
