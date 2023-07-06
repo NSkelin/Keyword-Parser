3e5e71c01b9718d5c6803b022075423d04052a0d
Nick <nick.skelin@gmail.com>
2023-07-06 14:30:15 -0700

    Add story

---
+++ src/app/Components/KeywordItem/KeywordItem.stories.ts
@@ -0,0 +1,17 @@
+import {Meta, StoryObj} from "@storybook/react";
+import KeywordItem from "./KeywordItem";
+
+const meta: Meta<typeof KeywordItem> = {
+  component: KeywordItem,
+};
+
+export default meta;
+type Story = StoryObj<typeof KeywordItem>;
+
+export const Default: Story = {
+  args: {
+    instances: 5,
+    highlightPercent: 20,
+    name: "keyword",
+  },
+};
