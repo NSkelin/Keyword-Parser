8805a25571ede527657744288d1744e8096d24e2
Nick <nick.skelin@gmail.com>
2023-07-07 11:37:18 -0700

    add documentation

--- src/app/Components/CommaSeparatedInput/CommaSeparatedInput.tsx
+++ src/app/Components/CommaSeparatedInput/CommaSeparatedInput.tsx
@@ -1,10 +1,19 @@
 import React, {ChangeEvent, useState} from "react";
 import styles from "./CommaSeparatedInput.module.scss";
 
-export type CommaSeparatedInputProps = {label: string; savedInputs: string[]; onInputChange: (savedInputs: string[]) => void};
+export type CommaSeparatedInputProps = {
+  /** Label for the input */
+  label?: string;
+  /** The list of words shown below the input */
+  savedInputs: string[];
+  /** Callback for when a user adds or removes a word that should be used to update state */
+  onInputChange: (savedInputs: string[]) => void;
+};
+/** Renders an \<input> used for displaying and editing a list of words. When a user types "," a new word will be added to the list. */
 function CommaSeparatedInput({label, savedInputs, onInputChange}: CommaSeparatedInputProps) {
   const [inputValue, setInputValue] = useState("");
 
+  /** Handles the response to a user typing in the input. When the user enters a comma, the input will be cleared and a new item will be added to the list below. */
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const newVal = e.target.value;
     if (newVal.slice(-1) === ",") {
@@ -16,10 +25,12 @@ function CommaSeparatedInput({label, savedInputs, onInputChange}: CommaSeparated
     return;
   }
 
+  /** Handles the response to a user deleting an item from the list, by deleting the item. */
   function handleDelete(val: string) {
     onInputChange(savedInputs.toSpliced(savedInputs.indexOf(val), 1));
   }
 
+  // Creates the display of items under the input.
   const savedInputsDisplay = savedInputs.map((val, index) => (
     <span key={index}>
       {val}
