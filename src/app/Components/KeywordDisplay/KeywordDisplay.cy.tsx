3c5aa5f40580649ca60c38729dda9e2845f8ddae
Nick <nick.skelin@gmail.com>
2023-08-22 13:09:31 -0700

    add tests and refactor test structure

--- src/app/Components/KeywordDisplay/KeywordDisplay.cy.tsx
+++ src/app/Components/KeywordDisplay/KeywordDisplay.cy.tsx
@@ -17,57 +17,90 @@ describe("<KeywordDisplay />", () => {
     );
   });
 
-  describe("Form in 'edit' mode changes should reset upon canceling", () => {
+  describe("Form in 'edit' mode", () => {
     beforeEach(() => {
       // open form in edit mode
       cy.get("[data-cy='edit']").eq(0).click();
+    });
+    describe("should close", () => {
+      afterEach(() => {
+        cy.get("[data-cy='dialogComp']").should("not.be.visible");
+      });
 
-      // create aliases for form inputs & controls
-      cy.get("[data-cy='keywordEditorComp']").within(() => {
-        cy.get("[data-cy='input']").eq(0).as("displayName");
-        cy.get("[data-cy='proficient']").as("proficient");
-        cy.get("[data-cy='commaSeparatedInputComp']").as("CSI");
-        cy.get("[data-cy='input']").eq(1).as("aliases");
-        cy.get("[data-cy='submit']").as("submit");
-        cy.get("[data-cy='delete']").as("delete");
-        cy.get("[data-cy='cancel']").as("cancel");
+      it("on submit", () => {
+        cy.get("[data-cy='keywordEditorComp']").find("[data-cy='submit']").click();
+      });
+
+      it("on delete", () => {
+        cy.get("[data-cy='keywordEditorComp']").find("[data-cy='delete']").click();
+      });
+
+      it("on cancel", () => {
+        cy.get("[data-cy='keywordEditorComp']").find("[data-cy='cancel']").click();
       });
     });
 
-    it("should not show changed displayName after canceling", () => {
-      cy.get("@displayName").clear().type("testDisplayName");
-      cy.get("@cancel").click();
+    describe("should not close", () => {
+      afterEach(() => {
+        cy.get("[data-cy='dialogComp']").should("be.visible");
+      });
 
-      // reopen form
-      cy.get("[data-cy='edit']").eq(0).click();
-      cy.get("@displayName").invoke("val").should("not.equal", "testDisplayName");
+      it("on failed submit", () => {
+        cy.get("[data-cy='input']").eq(0).clear();
+        cy.get("[data-cy='keywordEditorComp']").find("[data-cy='submit']").click();
+      });
     });
 
-    it("should show deleted aliases after canceling", () => {
-      cy.get("@CSI").find("[data-cy='remove']").eq(0).click();
-      cy.get("@cancel").click();
+    // if the user edits an input and cancels their changes it should not show those changes when reopening the form
+    describe("should reset form input changes after canceling", () => {
+      beforeEach(() => {
+        // create aliases for form inputs & controls
+        cy.get("[data-cy='keywordEditorComp']").within(() => {
+          cy.get("[data-cy='input']").eq(0).as("displayName");
+          cy.get("[data-cy='proficient']").as("proficient");
+          cy.get("[data-cy='commaSeparatedInputComp']").as("CSI");
+          cy.get("[data-cy='input']").eq(1).as("aliases");
+          cy.get("[data-cy='submit']").as("submit");
+          cy.get("[data-cy='delete']").as("delete");
+          cy.get("[data-cy='cancel']").as("cancel");
+        });
+      });
 
-      // reopen form
-      cy.get("[data-cy='edit']").eq(0).click();
-      cy.get("@CSI").contains("span", "kw4").should("exist");
-    });
+      it("reset displayName", () => {
+        cy.get("@displayName").clear().type("testDisplayName");
+        cy.get("@cancel").click();
 
-    it("should not show new aliases after canceling", () => {
-      cy.get("@aliases").type("keyword 11,");
-      cy.get("@cancel").click();
+        // reopen form
+        cy.get("[data-cy='edit']").eq(0).click();
+        cy.get("@displayName").invoke("val").should("not.equal", "testDisplayName");
+      });
 
-      // reopen form
-      cy.get("[data-cy='edit']").eq(0).click();
-      cy.get("@CSI").contains("span", "keyword 11").should("not.exist");
-    });
+      it("reset deleted aliases", () => {
+        cy.get("@CSI").find("[data-cy='remove']").eq(0).click();
+        cy.get("@cancel").click();
 
-    it("should not show the changed proficiency", () => {
-      cy.get("@proficient").click();
-      cy.get("[data-cy='cancel']").click();
+        // reopen form
+        cy.get("[data-cy='edit']").eq(0).click();
+        cy.get("@CSI").contains("span", "kw4").should("exist");
+      });
 
-      // reopen form
-      cy.get("[data-cy='edit']").eq(0).click();
-      cy.get("[data-cy='proficient']").should("not.be.checked");
+      it("reset created aliases", () => {
+        cy.get("@aliases").type("keyword 11,");
+        cy.get("@cancel").click();
+
+        // reopen form
+        cy.get("[data-cy='edit']").eq(0).click();
+        cy.get("@CSI").contains("span", "keyword 11").should("not.exist");
+      });
+
+      it("reset proficiency", () => {
+        cy.get("@proficient").click();
+        cy.get("[data-cy='cancel']").click();
+
+        // reopen form
+        cy.get("[data-cy='edit']").eq(0).click();
+        cy.get("[data-cy='proficient']").should("not.be.checked");
+      });
     });
   });
 });
