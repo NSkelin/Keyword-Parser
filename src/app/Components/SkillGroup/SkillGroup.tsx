6606026ad13672a297ac0857459d405a7718f488
Nick <nick.skelin@gmail.com>
2023-08-24 16:00:24 -0700

    add toggle to control whats copied to clipboard
    Each skill group now has a toggle that controls if it will be copied to
    the clipboard or not.

--- src/app/Components/SkillGroup/SkillGroup.tsx
+++ src/app/Components/SkillGroup/SkillGroup.tsx
@@ -6,13 +6,15 @@ import styles from "./SkillGroup.module.scss";
 export interface SkillGroupProps {
   title: string;
   skills: SkillItemProps[];
+  selected: boolean;
+  onChange: (title: string) => void;
 }
-function SkillGroup({title, skills}: SkillGroupProps) {
+function SkillGroup({title, skills, selected, onChange}: SkillGroupProps) {
   const wordsColored = skills.map(({name, color}) => <SkillItem key={name} name={name} color={color} />);
 
   return (
     <div>
-      <input type="checkbox" defaultChecked={true} />
+      <input type="checkbox" checked={selected} onChange={() => onChange(title)} />
       <b> {title}:</b>
       {wordsColored}
     </div>
--- src/app/Components/SkillSummary/SkillSummary.tsx
+++ src/app/Components/SkillSummary/SkillSummary.tsx
@@ -1,28 +1,57 @@
 import {SkillGroup} from "@/components";
-import React from "react";
+import React, {useState} from "react";
 import type {SkillGroupProps} from "../SkillGroup";
+import {SkillItemProps} from "../SkillItem";
 
 export interface KeywordSummaryProps {
   skillGroups: SkillGroupProps[];
 }
 function SkillSummary({skillGroups}: KeywordSummaryProps) {
-  const skillGroupElements = skillGroups.map(({title, skills}, index) => {
-    return (
-      <div key={index}>
-        <SkillGroup title={title} skills={skills} />
-      </div>
-    );
-  });
+  const [selectedGroups, setSelectedGroups] = useState<Map<string, boolean>>(test());
 
+  function test() {
+    const m = new Map();
+    for (const group of skillGroups) {
+      m.set(group.title, true);
+    }
+    return m;
+  }
+
+  function toggleGroup(title: string) {
+    const group = selectedGroups.get(title);
+    selectedGroups.set(title, !group);
+    setSelectedGroups(new Map(selectedGroups));
+  }
+
+  /** Converts the skill summary data into a string and copys to clipboard */
   function copyToClipboard() {
-    const skillGroupArray = skillGroups.map(({title, skills}) => {
-      const groupSkills = skills.map(({name}) => name);
-      return `${title}: ${groupSkills.join(", ") + "."}`;
-    });
+    // reduces the skills into a single string
+    function createSkillString(skills: SkillItemProps[]) {
+      const skillsString = skills.reduce<string>((accumulator, {name}, index) => {
+        // place a period after the last skill instead of a comma
+        if (index === skills.length - 1) {
+          return accumulator + name + ".";
+        } else {
+          return accumulator + name + ", ";
+        }
+      }, "");
+      return skillsString;
+    }
 
-    const skillString = skillGroupArray.join("\n");
+    // reduce the selected skill groups into a single string
+    const skillSummaryString = skillGroups.reduce<string>((accumulator, {title, skills}) => {
+      if (selectedGroups.get(title)) {
+        const skillsString = createSkillString(skills);
+        if (accumulator === "") {
+          return accumulator + `${title}: ${skillsString}`;
+        } else {
+          return accumulator + `\n${title}: ${skillsString}`;
+        }
+      }
+      return accumulator;
+    }, "");
 
-    navigator.clipboard.writeText(skillString).then(
+    navigator.clipboard.writeText(skillSummaryString).then(
       () => {
         // TODO ~
         // Success ~
@@ -37,6 +66,14 @@ function SkillSummary({skillGroups}: KeywordSummaryProps) {
       },
     );
   }
+
+  const skillGroupElements = skillGroups.map(({title, skills}, index) => {
+    return (
+      <div key={index}>
+        <SkillGroup title={title} skills={skills} selected={selectedGroups.get(title) ?? false} onChange={toggleGroup} />
+      </div>
+    );
+  });
 
   return (
     <section>
