a02bef735ab308bb67258b7389ebf1f73532b72f
Nick <nick.skelin@gmail.com>
2024-12-15 22:47:15 -0800

    replace module imports with optimize package import

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  modularizeImports: {
-    "@/components/(((\\w*)?/?)*)": {
-      transform: "@/components/{{matches.[1]}}/{{matches.[1]}}",
-      skipDefaultConversion: true,
-    },
-    "@/components": {
-      transform: "@/components/{{member}}/{{member}}",
-    },
-    "@/utils": {
-      transform: "@/utils/utils",
-      skipDefaultConversion: true,
-    },
+  experimental: {
+    optimizePackageImports: ["@/components", "@/utils"],
   },
 };
 
