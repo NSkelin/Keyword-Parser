27b92ea5843ec0753fde34cfd62f8f89fa0298ba
Nick <nick.skelin@gmail.com>
2024-12-18 03:23:16 -0800

    prepare for eslint 9
    Added a flat config file as it will become default in v9.
    Added packages required to make the flat config work.
    Eslint is using the new config but eslintrc is left in for nextjs until i am able to update to next15 where the new config is supported.

---
+++ eslint.config.mjs
@@ -0,0 +1,82 @@
+// @ts-check
+
+import {FlatCompat} from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import jest from "eslint-plugin-jest";
+import storybook from "eslint-plugin-storybook";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: eslint.configs.recommended,
+  allConfig: eslint.configs.all,
+});
+
+const eslintConfig = tseslint.config([
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  jest.configs["flat/recommended"],
+  jest.configs["flat/style"],
+  prettier,
+  ...storybook.configs["flat/recommended"],
+  {
+    ignores: ["!**/.storybook", ".next", "cypress"],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    languageOptions: {
+      globals: {...jest.environments.globals.globals, cy: "writable"},
+      ecmaVersion: "latest",
+      sourceType: "script",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: "__dirname",
+      },
+    },
+
+    rules: {
+      camelcase: ["error", {properties: "always"}],
+    },
+  },
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+  {
+    // Allow empty functions because i am not going to mock them.
+    files: ["**/*.stories.*", "**/*.cy.*"],
+    rules: {
+      "@typescript-eslint/no-empty-function": "off",
+    },
+  },
+  {
+    files: ["**/*.test.*", "**/*.cy.*"],
+    rules: {
+      "jest/consistent-test-it": ["error", {fn: "it"}],
+    },
+  },
+  {
+    // false warning because .cy. files are cypress not jest.
+    files: ["**/*.cy.*"],
+    rules: {
+      "jest/expect-expect": "off",
+    },
+  },
+  {
+    // This is how cypress does it by default and im not changing it
+    files: ["./cypress/support/component.ts"],
+    rules: {
+      "@typescript-eslint/no-namespace": ["error", {allowDeclarations: true}],
+    },
+  },
+]);
+
+export default eslintConfig;
