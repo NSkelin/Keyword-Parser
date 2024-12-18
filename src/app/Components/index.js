e5993e44bee9dc7842e329a94a3c3f44e4f1d92a
Nick <nick.skelin@gmail.com>
2024-12-18 02:27:41 -0800

    fix import

--- src/app/Components/index.js
+++ src/app/Components/index.js
@@ -1,6 +1,6 @@
 export {default as BulletList} from "./BulletList";
 export {default as CommaSeparatedInput} from "./CommaSeparatedInput";
-export {default as Dialog} from "./Dialog/Dialog";
+export {default as Dialog} from "./Dialog";
 export {default as Input} from "./Input";
 export {default as KeywordDisplay} from "./KeywordDisplay";
 export {default as KeywordDisplayCollection} from "./KeywordDisplayCollection";
