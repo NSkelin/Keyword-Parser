985381c3636f091a047b7439b5b851473754b9b0
Nick <nick.skelin@gmail.com>
2024-12-26 21:41:40 -0800

    update rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -44,6 +44,18 @@ const eslintConfig = tseslint.config([
 
     rules: {
       camelcase: ["error", {properties: "always"}],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
   {
