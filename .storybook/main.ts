e2892506fea54a9d470644c3f06e6c8192eb0fff
Nick <nick.skelin@gmail.com>
2025-01-13 16:50:18 -0800

    simplify config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,7 @@ import type {StorybookConfig} from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/app/components/**/*.mdx", "../src/app/components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
+  framework: "@storybook/nextjs",
   typescript: {
     reactDocgen: "react-docgen-typescript",
   },
