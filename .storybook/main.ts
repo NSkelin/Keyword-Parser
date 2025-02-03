8dc7c79075e6ef4781a525b752bf21002d4a0699
Nick <nick.skelin@gmail.com>
2025-02-03 13:11:46 -0800

    fix storybook searching wrong directory

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type {StorybookConfig} from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ["../src/app/components/**/*.mdx", "../src/app/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
   framework: "@storybook/nextjs",
   typescript: {
