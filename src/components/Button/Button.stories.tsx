337552a19d0ba5f54b73f9557c8d7988b2b06c5b
Nick <nick.skelin@gmail.com>
2025-02-03 17:12:45 -0800

    create generic button component for styling consistency

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,56 @@
+import {Meta, StoryObj} from "@storybook/react";
+import Image from "next/image";
+import {Button} from "./Button";
+
+const trashSVG = <Image src="/delete_forever.svg" alt="Edit icon" width={20} height={20} />;
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    buttonStyle: "default",
+    children: <>Button</>,
+  },
+};
+
+export const Submit: Story = {
+  args: {
+    buttonStyle: "submit",
+    children: <>Button</>,
+  },
+};
+
+export const Delete: Story = {
+  args: {
+    buttonStyle: "delete",
+    children: <>Button</>,
+  },
+};
+
+export const Create: Story = {
+  args: {
+    buttonStyle: "create",
+    children: <>Button</>,
+  },
+};
+
+export const IconOnly: Story = {
+  args: {
+    buttonStyle: "delete",
+    iconOnly: true,
+    children: <>{trashSVG}</>,
+  },
+};
+
+export const StretchedWidth: Story = {
+  args: {
+    buttonStyle: "create",
+    stretch: "width",
+    children: <>Button</>,
+  },
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,43 @@
+import {ButtonHTMLAttributes, DetailedHTMLProps} from "react";
+import styles from "./Button.module.scss";
+
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  /**
+   * Determines the buttons styling.
+   * @default "default"
+   */
+  buttonStyle?: "default" | "submit" | "delete" | "create";
+  /**
+   * True makes the padding on all sides equal, results in a square button instead of a rectangular button.
+   * @default false
+   */
+  iconOnly?: boolean;
+  /**
+   * Should the button stretch to it fill its container.
+   * @default "none"
+   */
+  stretch?: "none" | "width" | "height" | "both";
+  children?: React.ReactNode;
+}
+
+/**
+ * A generic button component to keep unified styling across all buttons.
+ */
+export function Button({buttonStyle = "default", iconOnly = false, stretch = "none", children, ...props}: ButtonProps) {
+  const iconStyle = iconOnly ? "icon" : "";
+  const stretchedStyle = `${stretch}Stretched`;
+
+  // space after each line is required to work.
+  const classNames = `
+  ${styles[buttonStyle]} 
+  ${styles[iconStyle]} 
+  ${styles[stretchedStyle]}
+  `;
+  return (
+    <>
+      <button className={classNames} {...props}>
+        {children}
+      </button>
+    </>
+  );
+}
