3ddf67694f6a9e19f9e38f3032c314700bf325ba
Nick <nick.skelin@gmail.com>
2025-02-05 15:26:03 -0800

    refactor to use mockdata file instead of hardcoding data

--- src/components/KeywordDisplay/KeywordDisplay.stories.ts
+++ src/components/KeywordDisplay/KeywordDisplay.stories.ts
@@ -1,3 +1,4 @@
+import {getMockKeywords} from "@/mockData/keywords";
 import {Meta, StoryObj} from "@storybook/react";
 import {KeywordDisplay} from "./KeywordDisplay";
 
@@ -8,74 +9,12 @@ const meta: Meta<typeof KeywordDisplay> = {
 export default meta;
 type Story = StoryObj<typeof KeywordDisplay>;
 
+const keywords = getMockKeywords().slice(18);
+
 export const Default: Story = {
   args: {
-    title: "qui quis",
-    keywords: [
-      {
-        id: 0,
-        aliases: ["Burris", "May"],
-        instances: 3,
-        proficient: true,
-        displayName: "Sheppard",
-      },
-      {
-        id: 1,
-        aliases: ["Katy", "Stella", "Angelique"],
-        instances: 9,
-        proficient: true,
-        displayName: "Dixon",
-      },
-      {
-        id: 2,
-        aliases: ["Nanette"],
-        instances: 7,
-        proficient: true,
-        displayName: "Wilkins",
-      },
-      {
-        id: 3,
-        aliases: ["Castro"],
-        instances: 2,
-        proficient: false,
-        displayName: "Acevedo",
-      },
-      {
-        id: 4,
-        aliases: ["Herrera"],
-        instances: 7,
-        proficient: false,
-        displayName: "Ophelia",
-      },
-      {
-        id: 5,
-        aliases: ["Eula"],
-        instances: 7,
-        proficient: false,
-        displayName: "Hester",
-      },
-      {
-        id: 6,
-        aliases: ["Lindsey", "Gwendolyn", "Johns"],
-        instances: 9,
-        proficient: true,
-        displayName: "Denise",
-      },
-      {
-        id: 7,
-        aliases: ["Stephens", "Dionne", "Erin"],
-        instances: 4,
-        proficient: true,
-        displayName: "Kari",
-      },
-      {
-        id: 8,
-        aliases: ["Latisha", "Yang", "Ginger"],
-        instances: 5,
-        proficient: true,
-        displayName: "Bette",
-      },
-    ],
+    title: "Names",
+    keywords,
     highlightColor: "lightgoldenrodyellow",
   },
 };
--- src/components/KeywordDisplayCollection/KeywordDisplayCollection.stories.ts
+++ src/components/KeywordDisplayCollection/KeywordDisplayCollection.stories.ts
@@ -1,3 +1,4 @@
+import {getMockKeywords} from "@/mockData/keywords";
 import {Meta, StoryObj} from "@storybook/react";
 import {KeywordDisplayCollection} from "./KeywordDisplayCollection";
 
@@ -8,167 +9,24 @@ const meta: Meta<typeof KeywordDisplayCollection> = {
 export default meta;
 type Story = StoryObj<typeof KeywordDisplayCollection>;
 
-export const Default: Story = {
+const keywords = getMockKeywords();
+
+export const MultipleCollections: Story = {
   args: {
     displays: [
-      {
-        highlightColor: "lightgreen",
-        keywords: [
-          {
-            id: 1,
-            displayName: "Juliana",
-            instances: 7,
-            proficient: true,
-            aliases: ["nostrud", "labore", "sit", "non", "occaecat"],
-          },
-          {
-            id: 2,
-            displayName: "Sweet",
-            instances: 5,
-            proficient: false,
-            aliases: ["fugiat", "eiusmod", "labore", "laborum", "aute"],
-          },
-          {
-            id: 3,
-            displayName: "Hahn",
-            instances: 8,
-            proficient: true,
-            aliases: ["aliquip", "nulla", "aute", "nulla"],
-          },
-          {
-            id: 4,
-            displayName: "Richardson",
-            instances: 5,
-            proficient: true,
-            aliases: ["duis", "pariatur", "irure", "est", "occaecat"],
-          },
-          {
-            id: 5,
-            displayName: "Walton",
-            instances: 9,
-            proficient: true,
-            aliases: ["ex", "consequat", "in", "anim", "ipsum"],
-          },
-          {
-            id: 6,
-            displayName: "Mcdowell",
-            instances: 2,
-            proficient: false,
-            aliases: ["magna", "ullamco", "sit", "veniam", "non"],
-          },
-          {
-            id: 7,
-            displayName: "Lane",
-            instances: 4,
-            proficient: true,
-            aliases: ["culpa", "excepteur", "veniam", "laborum", "elit"],
-          },
-          {
-            id: 8,
-            displayName: "Edwards",
-            instances: 6,
-            proficient: false,
-            aliases: ["esse", "Lorem", "dolor", "consectetur", "magna"],
-          },
-        ],
-        title: "Display 1",
-      },
-      {
-        highlightColor: "lightblue",
-        keywords: [
-          {
-            id: 1,
-            displayName: "Dorothy",
-            instances: 1,
-            proficient: true,
-            aliases: ["Lorem", "dolore", "labore"],
-          },
-          {
-            id: 2,
-            displayName: "Joyce",
-            instances: 3,
-            proficient: false,
-            aliases: ["Lorem", "anim"],
-          },
-          {
-            id: 3,
-            displayName: "Beatrice",
-            instances: 2,
-            proficient: true,
-            aliases: ["in", "ea", "ex", "pariatur", "ad"],
-          },
-          {
-            id: 4,
-            displayName: "Rhoda",
-            instances: 8,
-            proficient: true,
-            aliases: ["commodo", "esse"],
-          },
-          {
-            id: 5,
-            displayName: "Margarita",
-            instances: 2,
-            proficient: true,
-            aliases: ["laborum", "qui"],
-          },
-          {
-            id: 6,
-            displayName: "Robbins",
-            instances: 6,
-            proficient: false,
-            aliases: ["minim", "aliquip", "incididunt"],
-          },
-        ],
-        title: "Display 1",
-      },
-      {
-        highlightColor: "orange",
-        keywords: [
-          {
-            id: 1,
-            displayName: "Lancaster",
-            instances: 7,
-            proficient: true,
-            aliases: ["ipsum", "mollit"],
-          },
-          {
-            id: 2,
-            displayName: "Austin",
-            instances: 6,
-            proficient: true,
-            aliases: ["commodo", "voluptate"],
-          },
-          {
-            id: 3,
-            displayName: "Schwartz",
-            instances: 6,
-            proficient: false,
-            aliases: ["commodo", "occaecat", "ex", "enim", "ex"],
-          },
-          {
-            id: 4,
-            displayName: "Annette",
-            instances: 0,
-            proficient: false,
-            aliases: ["eiusmod", "labore", "magna", "sint"],
-          },
-          {
-            id: 5,
-            displayName: "Lynn",
-            instances: 3,
-            proficient: false,
-            aliases: ["amet", "reprehenderit", "est", "mollit", "non"],
-          },
-          {
-            id: 6,
-            displayName: "Cleveland",
-            instances: 4,
-            proficient: false,
-            aliases: ["cupidatat", "magna", "minim"],
-          },
-        ],
-        title: "Display 1",
-      },
+      {highlightColor: "lightgreen", keywords, title: "Favourites"},
+      {highlightColor: "lightblue", keywords, title: "Colleagues"},
+      {highlightColor: "orange", keywords, title: "Names"},
     ],
   },
+};
+
+export const SingleCollection: Story = {
+  args: {
+    displays: [{highlightColor: "lightgreen", keywords, title: "Display 1"}],
+  },
+};
+
+export const EmptyList: Story = {
+  args: {displays: []},
 };
