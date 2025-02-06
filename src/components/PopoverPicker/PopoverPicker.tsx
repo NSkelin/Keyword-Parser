f1eaade77e5f7f4b3a6903a93dd45279f5ce15cc
Nick <nick.skelin@gmail.com>
2025-02-06 15:28:52 -0800

    separate useEffect hook into custom hooks file

--- src/components/PopoverPicker/PopoverPicker.tsx
+++ src/components/PopoverPicker/PopoverPicker.tsx
@@ -1,40 +1,9 @@
 import {Button} from "@/components/Button";
+import {useClickOutside} from "@/customHooks";
 import Image from "next/image";
-import {useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 import {HexColorPicker} from "react-colorful";
 import styles from "./PopoverPicker.module.css";
-
-// Improved version of https://usehooks.com/useOnClickOutside/
-function useClickOutside(ref: React.RefObject<HTMLElement | null>, handler: (event: Event) => void) {
-  useEffect(() => {
-    let startedInside = false;
-    let startedWhenMounted = false;
-
-    const listener = (event: Event) => {
-      // Do nothing if `mousedown` or `touchstart` started inside ref element
-      if (startedInside || !startedWhenMounted) return;
-      // Do nothing if clicking ref's element or descendent elements
-      if (!ref.current || ref.current.contains(event.target as Node)) return;
-
-      handler(event);
-    };
-
-    const validateEventStart = (event: Event) => {
-      startedWhenMounted = !!ref.current;
-      startedInside = !!ref.current && ref.current.contains(event.target as Node);
-    };
-
-    document.addEventListener("mousedown", validateEventStart);
-    document.addEventListener("touchstart", validateEventStart);
-    document.addEventListener("click", listener);
-
-    return () => {
-      document.removeEventListener("mousedown", validateEventStart);
-      document.removeEventListener("touchstart", validateEventStart);
-      document.removeEventListener("click", listener);
-    };
-  }, [ref, handler]);
-}
 
 interface PopoverPickerProps {
   color: string;
