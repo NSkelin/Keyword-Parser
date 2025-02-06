67168a54e96aff604907bf1ff472d5fa584ea238
Nick <nick.skelin@gmail.com>
2025-02-06 15:32:09 -0800

    make clicking outside cancel the change

--- src/components/InlineEdit/InlineEdit.tsx
+++ src/components/InlineEdit/InlineEdit.tsx
@@ -1,6 +1,7 @@
 import {Button} from "@/components/Button";
+import {useClickOutside} from "@/customHooks";
 import Image from "next/image";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import styles from "./InlineEdit.module.scss";
 
 export interface InlineEditProps {
@@ -13,6 +14,9 @@ export interface InlineEditProps {
 export function InlineEdit({value, onSave, type = "text", children}: InlineEditProps) {
   const [isEditing, setIsEditing] = useState(false);
   const [inputValue, setInputValue] = useState(value);
+  const inlineEdit = useRef(null);
+
+  useClickOutside(inlineEdit, handleCancel);
 
   const editSVG = <Image src="/edit.svg" alt="Edit icon" width={16} height={16} />;
   const closeSVG = <Image src="/close.svg" alt="Edit icon" width={16} height={16} />;
@@ -31,7 +35,7 @@ export function InlineEdit({value, onSave, type = "text", children}: InlineEditP
   return (
     <div className={styles.container}>
       {isEditing ? (
-        <div className={styles.editingContainer}>
+        <div ref={inlineEdit} className={styles.editingContainer}>
           {type === "textarea" ? (
             <textarea value={inputValue} onChange={(e) => setInputValue(e.target.value)} autoFocus />
           ) : (
