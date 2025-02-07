6edb7b81dd7b3535b9b725de50e6b9163b73586c
Nick <nick.skelin@gmail.com>
2025-02-07 00:31:40 -0800

    add keypress shortcuts for saving and canceling

--- src/components/InlineEdit/InlineEdit.tsx
+++ src/components/InlineEdit/InlineEdit.tsx
@@ -1,12 +1,18 @@
 import {Button} from "@/components/Button";
 import {useClickOutside} from "@/customHooks";
 import Image from "next/image";
-import {useRef, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import styles from "./InlineEdit.module.scss";
 
 export interface InlineEditProps {
   value: string;
+  /**
+   * Ideally use a memoized function to prevent unnecessary eventlistener connection / disconnects.
+   */
   onSave: (newValue: string) => void;
+  /**
+   * Ideally use a memoized function to prevent unnecessary eventlistener connection / disconnects.
+   */
   onCancel?: () => void;
   type?: "text" | "number" | "textarea";
   children?: React.ReactNode;
@@ -17,22 +23,38 @@ export function InlineEdit({value, onSave, onCancel, type = "text", children}: I
   const [inputValue, setInputValue] = useState(value);
   const inlineEdit = useRef(null);
 
-  useClickOutside(inlineEdit, handleCancel);
-
   const editSVG = <Image src="/edit.svg" alt="Edit icon" width={16} height={16} />;
   const closeSVG = <Image src="/close.svg" alt="Edit icon" width={16} height={16} />;
   const checkSVG = <Image src="/check.svg" alt="Edit icon" width={16} height={16} />;
 
-  function handleSave() {
+  const handleSave = useCallback(() => {
     onSave(inputValue);
     setIsEditing(false);
-  }
+  }, [inputValue, onSave]);
 
-  function handleCancel() {
+  const handleCancel = useCallback(() => {
     setIsEditing(false);
     setInputValue(value);
     if (onCancel) onCancel();
-  }
+  }, [onCancel, value]);
+
+  useClickOutside(inlineEdit, handleCancel);
+  useEffect(() => {
+    // Save / close if enter / esc pressed while editing.
+    const listener = (event: KeyboardEvent) => {
+      if (!isEditing) return;
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        handleSave();
+      } else if (event.code === "Escape") {
+        handleCancel();
+      }
+    };
+
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [handleSave, handleCancel, isEditing, onCancel]);
 
   return (
     <div className={styles.container}>
