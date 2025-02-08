6a700e7245b8df148fceca3d5479f520e51f4419
Nick <nick.skelin@gmail.com>
2025-02-08 14:52:39 -0800

    add onCancel and onClose events

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -3,25 +3,49 @@ import styles from "./Dialog.module.scss";
 
 export interface DialogProps {
   open?: boolean;
+  onCancel?: (e: Event) => void;
+  onClose?: (e: Event) => void;
   /** React children */
   children: React.ReactNode;
   /** Dialog onClose event */
 }
 /** A html dialog wrapper */
-export function Dialog({open, children}: DialogProps) {
+export function Dialog({open, onCancel, onClose, children}: DialogProps) {
   const ref = useRef<HTMLDialogElement>(null);
 
+  // open/close dialog depending on open prop.
   useEffect(() => {
     if (!ref.current) return;
+    const dialog = ref.current;
 
-    if (open) {
-      if (!ref.current.open) {
-        ref.current.showModal();
-      }
-    } else {
-      ref.current.close();
+    if (open && !ref.current.open) {
+      dialog.showModal();
+    } else if (!open && dialog.open) {
+      dialog.close();
     }
   }, [open]);
+
+  // listen for and callback the dialogs cancel event
+  useEffect(() => {
+    if (!ref.current || !onCancel) return;
+    const dialog = ref.current;
+
+    dialog.addEventListener("cancel", onCancel);
+    return () => {
+      dialog.removeEventListener("cancel", onCancel);
+    };
+  }, [onCancel]);
+
+  // listen for and callback the dialogs close event
+  useEffect(() => {
+    if (!ref.current || !onClose) return;
+    const dialog = ref.current;
+
+    dialog.addEventListener("close", onClose);
+    return () => {
+      dialog.removeEventListener("close", onClose);
+    };
+  }, [onClose]);
 
   return (
     <dialog data-cy="dialogComp" ref={ref} className={styles.dialog}>
